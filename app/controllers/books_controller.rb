ef2a5774a411f74a5787df5b243d0993d3dbd1f2
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-02-09 16:52:07 +0000

    [Update] 4-25 ns:book show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,23 +6,45 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice]="You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
     @books = Book.page(params[:page])
+    @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def destroy
     @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
 
   private
   def book_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,31 @@ class UsersController < ApplicationController
   end
 
   def index
-    @user = User.all
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user =current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = user.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="You have update user successfully."
+      redirect_to user_path(current_user)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
